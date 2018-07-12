f7e38cd069504fccc82359ba17f38bbdc45bf71b
Bradley Bossard <bradleybossard@gmail.com>
2018-07-12 11:04:21 -0500

    Add html-webpack-plugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -20,5 +21,10 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 };
